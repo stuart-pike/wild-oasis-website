4eab137b6efed5e5db6cc43e2b36408c3a6ee967
stuart-pike <110692951+stuart-pike@users.noreply.github.com>
2024-09-03 14:59:38 -0700

    Organize project

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,3 +1,9 @@
-export default function Page() {
-  return <h1>Cabins page</h1>;
+import Counter from "@/app/_components/counter";
+
+export default async function Page() {
+  return (
+    <div>
+      <h1>Cabins page</h1>
+    </div>
+  );
 }
